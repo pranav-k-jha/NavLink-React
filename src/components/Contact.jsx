cd713076aa2c3efa4410218e09979e6b34ccd8d1
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-05-19 14:29:10 -0400

    added close logic

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,17 +5,25 @@ export default function Contact() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-    <h1>
-      Connect with us on{" "}
-      <a className="text-blue-600 hover:text-blue-800" href="https://linkedin.com" target="_blank">
-        LinkedIn
-      </a>
-    </h1>
-    <button onClick={() => setIsOpen(true)}> Open Popup</button>
-    <Modal  isOpen={isOpen}
-            setIsOpen={setIsOpen} header={<p className="text-red-400 font-bold text-xl">Hi</p>} footer={<button>OK</button>}>
-              <p>Test paragraph</p>
-            </Modal>
+      <h1>
+        Connect with us on{" "}
+        <a
+          className="text-blue-600 hover:text-blue-800"
+          href="https://linkedin.com"
+          target="_blank"
+        >
+          LinkedIn
+        </a>
+      </h1>
+      <button onClick={() => setIsOpen(true)}> Open Popup</button>
+      <Modal
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        header={<p className="text-xl font-bold text-red-400">Hi</p>}
+        footer={<button onClick={() => setIsOpen(false)}>OK</button>}
+      >
+        <p>Test paragraph</p>
+      </Modal>
     </>
   );
 }
