b1308ef2b94311e56b87d191212ac43cf83facbd
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-05-19 14:26:54 -0400

    updated Modal as reusable component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "./Modal";
 
 export default function Contact() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div>
+    <>
+    <h1>
       Connect with us on{" "}
       <a className="text-blue-600 hover:text-blue-800" href="https://linkedin.com" target="_blank">
         LinkedIn
       </a>
-    </div>
+    </h1>
+    <button onClick={() => setIsOpen(true)}> Open Popup</button>
+    <Modal  isOpen={isOpen}
+            setIsOpen={setIsOpen} header={<p className="text-red-400 font-bold text-xl">Hi</p>} footer={<button>OK</button>}>
+              <p>Test paragraph</p>
+            </Modal>
+    </>
   );
 }
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,31 +1,25 @@
-import React from 'react'
+import React from "react";
+import { createPortal } from "react-dom";
 
-export default function Modal() {
-  return (
-      <div className="fixed flex items-center px-4 justify-center inset-0 bg-black/40">
-      <div className="rounded-lg grow max-w-2xl bg-white p-4 shadow-lg">
-        <div className="text-xl font-bold">Sign In</div>
-        <div className="-mx-4 my-3 border-y px-4 py-4 flex flex-wrap gap-4">
-          <input
-            placeholder="Username"
-            className="grow rounded border border-gray-600 px-2 py-1"
-            type="text"
-          />
-          <input
-            placeholder="Password"
-            className="grow rounded border border-gray-600 px-2 py-1"
-            type="password"
-          />
+export default function Modal({ isOpen, setIsOpen, header, footer, children }) {
+  return createPortal(
+    <div
+      onClick={() => setIsOpen(false)}
+      className={`fixed inset-0 flex items-center justify-center bg-black/40 px-4 ${
+        isOpen ? "" : "hidden"
+      }`}
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="max-w-2xl grow rounded-lg bg-white p-4 shadow-lg"
+      >
+        {header}
+        <div className="-mx-4 my-3 flex flex-wrap gap-4 border-y px-4 py-4">
+          {children}
         </div>
-        <div className="flex justify-end gap-4">
-          <button className="rounded-md bg-gray-300 px-6 py-2 font-semibold hover:bg-gray-400/80 active:bg-gray-400/60">
-            Cancel
-          </button>
-          <button className="rounded-md bg-blue-300 px-6 py-2 font-semibold hover:bg-blue-400/80 active:bg-blue-400/60">
-            Sign In
-          </button>
-        </div>
+        {footer}
       </div>
-    </div>
-  )
+    </div>,
+    document.getElementById("portal")
+  );
 }
